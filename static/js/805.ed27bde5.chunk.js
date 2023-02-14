95cf2df1b1c2451f5f58b5f69d1acf37dff60e24
DimaMarkow <108217740+DimaMarkow@users.noreply.github.com>
2023-02-14 14:10:32 +0000

    Deploying to gh-pages from @ DimaMarkow/goit-react-hw-05-movies@36338c0da16909e5c0bcade6b9844710dda2f87c 🚀

---
+++ static/js/805.ed27bde5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,n){n.r(t);var r=n(791),c=n(689),s=n(184);t.default=function(){var e=(0,c.s0)();return(0,r.useEffect)((function(){setTimeout((function(){e("/")}),1500)}),[e]),(0,s.jsxs)("main",{style:{textAlign:"center"},children:[(0,s.jsx)("b",{style:{fontSize:48},children:"404"}),(0,s.jsx)("p",{children:"Sorry, we couldn't find that page..."})]})}}}]);
+//# sourceMappingURL=805.ed27bde5.chunk.js.map
